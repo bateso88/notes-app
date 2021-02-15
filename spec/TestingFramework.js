eb218fec0256c0125587357126d9776818e4d565
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-02-15 16:17:42 +0000

    Created test for not expected

--- spec/TestingFramework.js
+++ spec/TestingFramework.js
@@ -1,21 +1,36 @@
 var expected = 3;
+var notExpected = 3;
 var number_1 = 1;
 var number_2 = 2;
 
 function sum(a, b) {
-  return a + b
+  return a + b;
 }
 
 function subtract(a, b) {
-  return a - b
+  return a - b;
 }
 
-function testEqual (function_call, expected) {
-  let actual = function_call
+function testName(string) {
+  console.log(string);
+}
+
+function testEqual(function_call, expected) {
+  let actual = function_call;
   if (actual === expected) {
-    console.log('Pass')
+    console.log("Pass");
   } else {
-    console.log('Fail')
-    console.log(`Expected to get ${expected}, but got ${actual}`)
+    console.log("Fail");
+    console.log(`Expected to get ${expected}, but got ${actual}`);
+  }
+}
+
+function testNotEqual(function_call, notExpected) {
+  let actual = function_call;
+  if (actual != notExpected) {
+    console.log("Pass");
+  } else {
+    console.log("Fail");
+    console.log(`Expected not to get ${notExpected}, but got ${actual}`);
   }
 }
