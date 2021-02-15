e50c6bb77de00460952ba89a8fa21b90f66121cd
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-02-15 15:17:38 +0000

    testEqual function created returning Pass or Fail

--- spec/TestingFramework.js
+++ spec/TestingFramework.js
@@ -1,3 +1,15 @@
+var expected = 3;
+var number_1 = 1;
+var number_2 = 2;
+
 function sum(a, b) {
   return a + b
+}
+
+function testEqual (number_1, number_2, expected) {
+  if (sum(number_1, number_2) === expected) {
+    console.log('Pass')
+  } else {
+    console.log('Fail')
+  }
 }
