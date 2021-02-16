4276f040e96e2172851cec6d5c2e62b543b1dc39
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-02-16 16:30:01 +0000

    Abbreviated text to 20 characters

--- Notebook.js
+++ Notebook.js
@@ -1,22 +1,46 @@
 class Notebook {
-    constructor() {
-        this.notes = []
-    }
+  constructor() {
+    this.notes = [];
+    this.links = [];
+  }
 
-    newNote() {
-        return '<form id="note"><textarea id="notetext" cols="56" rows="8"></textarea><button type="submit">Submit</button></form>'
-    }
+  newNote() {
+    return '<form id="note"><textarea id="notetext" cols="56" rows="8"></textarea><button type="submit">Submit</button></form>';
+  }
 
-    addNote(note) {
-        this.notes.push(note);
-        // console.log(this.notes);
-    }
+  addNote(note) {
+    this.notes.push(note);
+    // console.log(this.notes);
+  }
 
-    printNotes() {
-        let x = this.notes.map(note => `<p>${note}</p>`).join('')
-        console.log(x);
-        return x;
-        // console.log(x.join())
-        // return x.join('');
-    }
+  addLink(link) {
+    this.links.push(this.abbreviateNote(link));
+    // console.log(this.notes);
+  }
+
+  printNotes() {
+    let x = this.notes.map((note) => `<p>${note}</p>`).join("");
+    // console.log(x);
+    return x;
+    // console.log(x.join())
+    // return x.join('');
+  }
+
+  printLinks() {
+    let x = this.links
+      .map((link) => `<a href="index.html">${link}</a></br>`)
+      .join("");
+    // console.log(x);
+    return x;
+    // console.log(x.join())
+    // return x.join('');
+  }
+
+  printAll() {
+    return this.printLinks() + this.printNotes();
+  }
+
+  abbreviateNote(note) {
+    return note.substring(0, 20);
+  }
 }
--- interface.js
+++ interface.js
@@ -1,43 +1,39 @@
 function interface() {
-    let root = document.getElementById('root');
-    
-    let notebook = new Notebook();
-    
-    // root.innerHTML = createHeader() + createMainContent() + createFooter();
-    root.innerHTML = createNoteCapture() + createMainContent();
-     
-    let capture = document.getElementById('note-capture')
+  let root = document.getElementById("root");
 
-    capture.innerHTML = notebook.newNote();
+  let notebook = new Notebook();
 
-    let note = document.getElementById('note');
+  // root.innerHTML = createHeader() + createMainContent() + createFooter();
+  root.innerHTML = createNoteCapture() + createMainContent();
 
-    note.addEventListener('submit', event => {
-        event.preventDefault();
-        console.log(event);
-        console.log(notetext)
-        console.log(notetext.value)
-        notebook.addNote(notetext.value);
-        document.getElementById('main-content').innerHTML = notebook.printNotes();
-    })
+  let capture = document.getElementById("note-capture");
 
-   
+  capture.innerHTML = notebook.newNote();
+
+  let note = document.getElementById("note");
+
+  note.addEventListener("submit", (event) => {
+    event.preventDefault();
+    notebook.addNote(notetext.value);
+    notebook.addLink(notetext.value);
+    document.getElementById("main-content").innerHTML = notebook.printAll();
+  });
 }
 
 interface();
 
 function createHeader() {
-    return '<div id="header"></div>'
+  return '<div id="header"></div>';
 }
 
 function createNoteCapture() {
-    return '<div id="note-capture"></div>'
+  return '<div id="note-capture"></div>';
 }
 
 function createMainContent() {
-    return '<div id="main-content"></div>'
+  return '<div id="main-content"></div>';
 }
 
 function createFooter() {
-    return '<div id="footer"></div>'
+  return '<div id="footer"></div>';
 }
